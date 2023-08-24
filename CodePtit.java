d23bef028f94b6e02e911e43cc8ce36118f16d24
NamNaha <112816849+namnezzzUvU@users.noreply.github.com>
2023-08-25 00:41:43 +0700

    Add files via upload

---
+++ CodePtit.java
@@ -0,0 +1,42 @@
+package codeptit;
+
+
+
+import java.util.Scanner;
+
+
+public class CodePtit {
+   
+   public static Scanner ip = new Scanner(System.in);
+   
+   public static int tmp = 0;
+   public static void sout(int[] arr, int n, int k) {
+       for(int i = 1;i<=k;++i) {
+           System.out.print(arr[i] + " ");
+       }
+        System.out.println("");
+   }
+   public static void genNext(int[] arr, int i,int n, int k) {
+      for(int j = arr[i-1] + 1; j<=n-k+i; ++j) {
+          arr[i] = j;
+          if(i==k) {
+              ++tmp;
+              sout(arr,n,k);
+          }
+          else {
+              genNext(arr,i+1, n, k);
+          }
+      }
+   }
+   
+   
+       
+    public static void main(String[] args) {
+        int n,k;
+        n = ip.nextInt();
+        k = ip.nextInt();
+        int[] arr = new int[1000];
+        genNext(arr, 1, n, k);
+        System.out.println("Tong cong co " + tmp + " to hop");
+    }
+}
